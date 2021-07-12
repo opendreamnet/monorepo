b88eb08aab9d82810f4c59e8be3e7be77300ca81
Ivan Bravo Bravo <kolessios@gmail.com>
2021-07-11 23:14:15 -0500

    fix: tooltips not visible when inside a <dialog>

--- workspaces/nuxtjs-base/mixins/BaseMixin.ts
+++ workspaces/nuxtjs-base/mixins/BaseMixin.ts
@@ -5,15 +5,21 @@ export default {
   directives: {
     tooltip: {
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      inserted(el: Element, binding: any): void {
+      inserted(el: HTMLElement, binding: any): void {
         if (!binding.value) {
           return
         }
+
+        const dialog = el.closest('dialog')
 
         let options: Partial<Props> = {}
 
         if (isString(binding.value)) {
           options.content = binding.value
+
+          if (dialog) {
+            options.appendTo = dialog
+          }
         } else {
           options = binding.value
         }
