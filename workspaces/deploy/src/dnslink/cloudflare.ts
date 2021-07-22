5456a90c516ffc3236310326df0564b84546526a
Ivan Bravo Bravo <kolessios@gmail.com>
2021-07-22 06:19:44 -0500

    fix: DNS step always created a record instead of updating the existing one.

--- workspaces/deploy/src/dnslink/cloudflare.ts
+++ workspaces/deploy/src/dnslink/cloudflare.ts
@@ -1,15 +1,43 @@
-// import cf from 'cloudflare'
-import updateDnsLink from 'dnslink-cloudflare'
+import axios, { AxiosInstance } from 'axios'
 import { isEmpty } from 'lodash'
-import { DnsRecord, DnsLink } from '../types'
+import { DnsRecord } from '../types'
 import { DnsProvider } from './base'
 
 export class Cloudflare extends DnsProvider {
+  /**
+   * Cloudflare email
+   *
+   * @protected
+   */
   protected _email?: string
 
+  /**
+   * Cloudflare key
+   *
+   * @protected
+   */
   protected _key?: string
 
+  /**
+   * Cloudflare token
+   *
+   * @protected
+   */
   protected _token?: string
+
+  /**
+   * HTTP client
+   *
+   * @protected
+   */
+  protected api?: AxiosInstance
+
+  /**
+   * Cloudflare zone id
+   *
+   * @protected
+   */
+  protected zoneId?: number
 
   public get email(): string | undefined {
     return this._email || process.env.DEPLOY_CLOUDFLARE_EMAIL
@@ -31,27 +59,33 @@ export class Cloudflare extends DnsProvider {
     return this._record || process.env.DEPLOY_CLOUDFLARE_RECORD
   }
 
-  public get api(): Record<string, string> {
-    const options: Record<string, string> = {}
-
+  /**
+   * Headers for the HTTP API.
+   *
+   * @readonly
+   */
+  public get headers(): Record<string, string> | undefined {
+    // Token
     if (this.token) {
-      options.token = this.token
-    } else if (this.email && this.key) {
-      options.email = this.email
-      options.key = this.key
+      return {
+        Authorization: `Bearer ${this.token}`
+      }
     }
 
-    return options
-  }
-
-  public get options(): DnsLink {
-    return {
-      zone: this.zone as string,
-      record: this.record as string,
-      link: `/ipfs/${this.release.cid}`,
+    // Key
+    if (this.email && this.key) {
+      return {
+        'X-Auth-Email': this.email,
+        'X-Auth-Key': this.key
+      }
     }
+
+    return undefined
   }
 
+  /**
+   * Validates the service information.
+   */
   public validate(): void {
     if ((isEmpty(this.key) && isEmpty(this.email)) && isEmpty(this.token)) {
       throw new Error(`Missing credentials:
@@ -71,12 +105,82 @@ DEPLOY_CLOUDFLARE_TOKEN`)
     }
   }
 
-  public async link(): Promise<DnsRecord> {
-    const content = await updateDnsLink(this.api, this.options)
+  /**
+   * Initializes the service.
+   */
+  public async setup(): Promise<void> {
+    this.api = axios.create({
+      baseURL: 'https://api.cloudflare.com/client/v4/',
+      headers: this.headers
+    })
+
+    this.zoneId = await this.getZoneId()
+  }
+
+  /**
+   * Execute DNSLink creation
+   */
+  public async exec(): Promise<DnsRecord> {
+    if (!this.api) {
+      throw new Error('Not setup')
+    }
+
+    const content = `dnslink=/ipfs/${this.release.cid}`
+    const recordId = await this.getRecordId()
+
+    if (recordId) {
+      await this.api.patch(`zones/${this.zoneId}/dns_records/${recordId}`, {
+        content
+      })
+    } else {
+      await this.api.post(`zones/${this.zoneId}/dns_records`, {
+        name: this.record,
+        type: 'TXT',
+        content
+      })
+    }
 
     return {
-      record: this.options.record,
-      content,
+      record: this.record as string,
+      content
     }
+  }
+
+  /**
+   * Get the zone id.
+   */
+  protected async getZoneId(): Promise<number> {
+    if (!this.api) {
+      throw new Error('Not setup')
+    }
+
+    const { data } = await this.api.get(`zones?name=${this.zone}`)
+
+    if (data.result.length === 0) {
+      throw new Error(`Zone ${this.zone} not found`)
+    }
+
+    return data.result[0].id
+  }
+
+  /**
+   * Get the DNS record id.
+   *
+   * @protected
+   */
+  protected async getRecordId(): Promise<number | undefined> {
+    if (!this.api) {
+      throw new Error('Not setup')
+    }
+
+    const { data } = await this.api.get(`zones/${this.zoneId}/dns_records?type=TXT`)
+
+    if (data.result.length === 0) {
+      return undefined
+    }
+
+    const record = data.result.find(record => record.name === `${this.record}.${this.zone}`)
+
+    return record ? record.id : undefined
   }
 }
